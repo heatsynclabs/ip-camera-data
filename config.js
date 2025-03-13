8a628f2706e23c5737f10ed4a1d3181716f003d6
Luis Montes <monteslu@gmail.com>
2025-03-13 12:40:22 -0700

    use env var port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  port: 8080,
+  port: process.env.PORT || 8080,
   CAM1: process.env.CAM1 || 'https://live.heatsynclabs.org/snapshot.php?camera=1',
   CAM2: process.env.CAM2 || 'https://live.heatsynclabs.org/snapshot.php?camera=2',
   CAM3: process.env.CAM3 || 'https://live.heatsynclabs.org/snapshot.php?camera=3',
